6f2321a44f2077c1b689e988af7e2950285be6e4
Jiseung23 <Jiseung23@gmail.com>
2025-06-10 18:31:39 +0900

    Update 00_주식데이터시각화.py

--- pages/00_주식데이터시각화.py
+++ pages/00_주식데이터시각화.py
@@ -1,1 +1,17 @@
+import streamlit as st
+import yfinance as yf
+import pandas as pd
+import matplotlib.pyplot as plt
+import plotly.express as px
+from datetime import datetime, timedelta
 
+st.set_page_config(layout="wide") # 넓은 레이아웃 설정
+
+st.title("글로벌 시총 Top 10 기업 주가 변화 시각화")
+
+# --- 여기에 기능 구현 ---
+
+if __name__ == '__main__':
+    # 스트림릿 애플리케이션 실행 명령어
+    # streamlit run app.py
+    pass
